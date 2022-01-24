6f8b22a3bb96b1ce03660a8772b06114eb48bc3e
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-24 14:09:14 -0500

    added basic functionality to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,58 +12,101 @@ const timerDisplay = document.getElementById("timer");
 var score;
 var userInitials;
 
-localStorage.setItem("score", score);//refer to in score page
+localStorage.setItem("score", score); //refer to in score page
 localStorage.setItem("userInitials", userInitials);
 
 // questions 
 const questionElement = document.getElementById("question")
-var questions [
-  {
-    question: "", // modifies questionElement
-    choice1: "", // modifies choiceButton1
-    choice2: "", // modifies choiceButton2
-    choice3: "", // modifies choiceButton3
-    choice4: "", // modifies choiceButton4
-    correct: "", // referenced for checkAnswer()
-  }
-  {}
-  {}
-  {}
-  {}
-  {}
-];
+
 // render question content in the html
-var questionIndex = 0; //references position of object in the question array starting with the first question
+let questions = [
+  {
+    question: "question1", // modifies questionElement
+    choice1: "choice1", // modifies choiceButton1
+    choice2: "choice2", // modifies choiceButton2
+    choice3: "choice3", // modifies choiceButton3
+    choice4: "choice4", // modifies choiceButton4
+    correct: "choice1", // referenced in checkAnswer()
+  },
+  {
+    question: "question2", // modifies questionElement
+    choice1: "choice1", // modifies choiceButton1
+    choice2: "choice2", // modifies choiceButton2
+    choice3: "choice3", // modifies choiceButton3
+    choice4: "choice4", // modifies choiceButton4
+    correct: "choice2", // referenced in checkAnswer()
+  },
+  {
+    question: "question3", // modifies questionElement
+    choice1: "choice1", // modifies choiceButton1
+    choice2: "choice2", // modifies choiceButton2
+    choice3: "choice3", // modifies choiceButton3
+    choice4: "choice4", // modifies choiceButton4
+    correct: "choice3", // referenced in checkAnswer()
+  },
+  {
+    question: "question4", // modifies questionElement
+    choice1: "choice1", // modifies choiceButton1
+    choice2: "choice2", // modifies choiceButton2
+    choice3: "choice3", // modifies choiceButton3
+    choice4: "choice4", // modifies choiceButton4
+    correct: "choice4", // referenced in checkAnswer()
+  },
+  {
+    question: "question5", // modifies questionElement
+    choice1: "choice1", // modifies choiceButton1
+    choice2: "choice2", // modifies choiceButton2
+    choice3: "choice3", // modifies choiceButton3
+    choice4: "choice4", // modifies choiceButton4
+    correct: "choice1", // referenced in checkAnswer()
+  }
+];
+
+let questionIndex = 0; //references position of object in the question array starting with the first question
+let currentQuestion = questions[questionIndex];
 function renderQuestion() {
-  var currentQuestion = questions[questionIndex];
-  questionElement.textContent = currentQuestion.question;
-  choiceButton1.textContent = currentQuestion.choice1;
-  choiceButton2.textContent = currentQuestion.choice2;
-  choiceButton3.textContent = currentQuestion.choice3;
-  choiceButton4.textContent = currentQuestion.choice4;
+  let currentQuestion = questions[questionIndex]; // why do I have to redeclare this?
+  questionElement.innerHTML = currentQuestion.question;
+  choiceButton1.innerHTML = currentQuestion.choice1;
+  choiceButton2.innerHTML = currentQuestion.choice2;
+  choiceButton3.innerHTML = currentQuestion.choice3;
+  choiceButton4.innerHTML = currentQuestion.choice4;
 }
 
-// for loop to cycle through questions 
-// probably won't work. look into other solutions
-for (var i=0; i < questions.length; i++){
-  // renderQuestion()?
-}
-
-// checks answer -> if (choice == correct)
-function checkAnswer() {}
+// log result of previous question in html 
+const result = document.getElementById("result")
 
 // section1 functionality: 
 function startQuiz() {
-  document.getElementById("#section1").hidden = true;
-  document.getElementById("#section2").hidden = false;
+  console.log("clicked");
+  renderQuestion();
+  document.getElementById("section1").hidden = true;
+  document.getElementById("section2").hidden = false;
+}
+// code to set time limit 
+
+// checks answer -> if (choice == correct)
+function checkAnswer(answer) {
+  let currentQuestion = questions[questionIndex]; // see line 68 comment. does this have to do with the expression?
+  console.log("clicked");
+  if (answer === currentQuestion.correct) {
+    console.log("Correct");
+    result.textContent = "Correct";
+  } else {
+    console.log("Incorrect");
+    result.textContent = "Incorrect"; 
+    // code to take 10 secconds off the timer
+  }
+  
+  if (questionIndex < questions.length-1) { //code to render next question in the array
+    console.log("next question")
+    questionIndex++;
+    renderQuestion();
+  } else {
+    console.log("no more questions")
+    return;
+  } // code to move to the submit form for scoring
 }
 
 // start quiz 
 startButton.addEventListener("click", startQuiz);
-console.log("clicked"); 
-
-// choiceButton functionality:
-choiceButton1.addEventListener("click", );
-choiceButton2.addEventListener("click", );
-choiceButton3.addEventListener("click", );
-choiceButton4.addEventListener("click", );
