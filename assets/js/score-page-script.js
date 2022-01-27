758b6fb6967681e85fc3f6da262f5fd5adcedba0
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-27 15:42:57 -0500

    better score log functionality

--- assets/js/score-page-script.js
+++ assets/js/score-page-script.js
@@ -1,11 +1,35 @@
 const scoresEl = document.getElementById("scores")
 let lastScore = JSON.parse(localStorage.getItem("loggedScore"));
 
-
-let userInitials = lastScore.initials;
-let score = lastScore.score;
-scoresEl.textContent = userInitials + ":" + score;
+let scoreLog = [
+  {
+    score: "55",
+    initials: "BL",
+  },
+]
 
 function clearScores(event) {
   scoresEl.textContent ="";
+  scoreLog = [];
+  localStorage.clear
 }
+
+// append loggedScore to scoreLog and sort in descending order by score
+function processScores () {
+  scoreLog.push(lastScore);
+  scoreLog.sort(function(a, b) {
+    return b.score - a.score
+  });
+  renderScores();
+}
+
+// render scores in the html
+function renderScores () {
+  scoreLog.forEach(function(element) {
+    let listEl = document.createElement("li");
+    listEl.textContent = element.initials + ": " + element.score;
+    scoresEl.appendChild(listEl);
+  })
+}
+
+processScores();
